45a10c6405ae04eada7cd8638d3c8ae5beb198bc
sroehrl <stefan.roehrl@gmail.com>
2020-06-23 23:16:18 -0400

    fix: run as global composer package

--- bin/initiate.php
+++ bin/initiate.php
@@ -1,6 +1,10 @@
 #!/usr/bin/env php
 <?php
-require __DIR__.'/../vendor/autoload.php';
+if (file_exists(__DIR__.'/../../../autoload.php')) {
+    require __DIR__.'/../../../autoload.php';
+} else {
+    require __DIR__.'/../vendor/autoload.php';
+}
 
 global $argv;
 $app = new \Cli\Cli($argv,getcwd());
