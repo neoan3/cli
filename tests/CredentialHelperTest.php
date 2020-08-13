ea64705f7ed9f9ff124f1f651ba9521e3e128970
sroehrl <stefan.roehrl@gmail.com>
2020-08-13 10:58:29 -0400

    Travis update

--- tests/CredentialHelperTest.php
+++ tests/CredentialHelperTest.php
@@ -10,7 +10,6 @@ class CredentialHelperTest extends TestCase
     public function testReadCredentials()
     {
         $h = new CredentialHelper(new MockCli(['neoan3','some','other'],dirname(__DIR__).'/playground'));
-        $h->readCredentials();
-        $this->assertIsArray($h->credentials);
+        $this->assertIsArray($h->readCredentials());
     }
 }
